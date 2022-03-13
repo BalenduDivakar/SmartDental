8ea0fa5b619631846071e52ea6d431bd68675a11
FlutLab.io <>
2022-03-13 11:33:50 +0000

    Initial commit

---
+++ lib/constants/app.dart
@@ -0,0 +1,15 @@
+const String baseUrl = 'https://smartdental.in/';
+
+const String consumerKey = 'ck_cbdd2edcbea570aa5a723a95d538c3f57b760611';
+
+const String consumerSecret = 'cs_867bb3579627faae45e71c0469689b70c126fd76';
+
+const String restPrefix = 'wp-json';
+
+const String defaultLanguage = 'en';
+
+const List<String> languageSupport = [
+  'en',
+];
+
+const String googleClientId = '295269595518-e7s01ueadskq7sbg2k4g4dfnefpmd7vt.apps.googleusercontent.com';
